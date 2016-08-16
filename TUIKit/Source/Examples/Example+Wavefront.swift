1fdc0d91a3b1b03d2faea2a73d527ca61e817575
African Swift <AfricanSwift@users.noreply.github.com>
2016-08-16 10:02:22 +0200

    Revert "Revert "Backfire culling for wavefront example""
    This reverts commit 1a14505bace6861561c433aa5b902d9201f43a42.

--- TUIKit/Source/Examples/Example+Wavefront.swift
+++ TUIKit/Source/Examples/Example+Wavefront.swift
@@ -28,36 +28,40 @@ public extension Example
       }
     }
     
-    struct Indices
+    struct Face
     {
-      let x: Int
-      let y: Int
-      let z: Int
+      let face: Int
+      let texture: Int
+      let vertex: Int
       
-      init(_ v: String)
+      private init(_ face: String, _ texture: String, _ vertex: String)
       {
-        let vertices = v.characters
-          .split(separator: "/", omittingEmptySubsequences: false)
-          .map { String($0) }
-        self.x = Int(vertices[0]) ?? -1
-        self.y = Int(vertices[1]) ?? -1
-        self.z = Int(vertices[2]) ?? -1
+        self.face = Int(face) ?? -1
+        self.texture = Int(texture) ?? -1
+        self.vertex = Int(vertex) ?? -1
+      }
+      
+      static func load(from line: String) -> [Face]
+      {
+        let step1 = line.characters.split(separator: " ")
+          .map { String($0).characters.split(separator: "/", omittingEmptySubsequences: false)
+            .map { String($0) } }
+        
+        return step1.flatMap { v -> Face? in
+          if String(v[0]).hasPrefix("f") { return nil }
+          let cnt = v.count
+          return Face(v[0], cnt > 1 ? v[1] : "", cnt > 2 ? v[2] : "")
+        }
       }
     }
     
     struct Polygon
     {
-      var indices: [Indices]
+      var faces: [Face]
       
       init(_ line: String)
       {
-        let indices: [Indices] = line.characters
-          .split(separator: " ", omittingEmptySubsequences: false).flatMap {
-            let v = String($0)
-            if v.hasPrefix("f") { return nil }
-            return Indices(v)
-        }
-        self.indices = indices
+        self.faces = Face.load(from: line)
       }
     }
     
@@ -97,8 +101,8 @@ public extension Example
       {
         for j in 0..<3
         {
-          let v0 = vertices[polygon.indices[j].x - 1]
-          let v1 = vertices[polygon.indices[(j+1)%3].x - 1]
+          let v0 = vertices[polygon.faces[j].face - 1]
+          let v1 = vertices[polygon.faces[(j+1)%3].face - 1]
           if v0.z < -0.3 || v1.z < -0.3 { continue }
           let pos0 = TUIVec2(x: (v0.x + 1) * center.x,
                              y: (v0.y + 1) * center.y)
@@ -110,78 +114,87 @@ public extension Example
       
       view.draw()
     }
+  }
+}
+
+public extension Example.Wavefront
+{
+  
+  public static func shoelace(_ v: [TUIVec2]) -> Double
+  {
+    let a = v[0].x * v[1].y - v[1].x * v[0].y
+    let b = v[1].x * v[2].y - v[2].x * v[1].y
+    let c = v[2].x * v[0].y - v[0].x * v[2].y
+    return (a + b + c) / 2
+  }
+  
+  public static func demo2() throws
+  {
+    Ansi.Set.cursorOff().stdout()
+    Ansi.Cursor.column().stdout()
+    guard let wSize = TUIWindow.ttysize() else { exit(EXIT_FAILURE) }
+    let width = Int(wSize.character.width * 2) - 4
+    let height = Int(wSize.character.height * 4) - 8 - 2 - 8
+    let param = TUIView.Parameter(invertYAxis: true)
+    var view = TUIView(x: 0, y: 0, width: width, height: height, parameters: param)
+    let center = TUIVec2(x: width / 2, y: height / 2)
+    var polygons = [Polygon]()
+    var vertices = [Vertice]()
+    let path = "head.obj"
+    let wfo = try String(contentsOfFile: path, encoding: .utf8)
+    let lines = wfo.characters
+      .split(separator: "\n", omittingEmptySubsequences: false)
+      .map { String($0) }
     
-    
-    public static func demo2() throws
+    for line in lines
     {
-      
-      Ansi.Set.cursorOff().stdout()
-      Ansi.Cursor.column().stdout()
-      guard let wSize = TUIWindow.ttysize() else { exit(EXIT_FAILURE) }
-      
-      let width = Int(wSize.character.width * 2) - 4
-      let height = Int(wSize.character.height * 4) - 8 - 2 - 8
-      let param = TUIView.Parameter(invertYAxis: true)
-      var view = TUIView(x: 0, y: 0, width: width, height: height, parameters: param)
-      let center = TUIVec2(x: width / 2, y: height / 2)
-      var polygons = [Polygon]()
-      var vertices = [Vertice]()
-      
-      let path = "african_head.obj"
-      let wfo = try String(contentsOfFile: path, encoding: .utf8)
-      let lines = wfo.characters
-        .split(separator: "\n", omittingEmptySubsequences: false)
-        .map { String($0) }
-      
-      for line in lines
+      if line.hasPrefix("f ")
       {
-        if line.hasPrefix("f ")
-        {
-          polygons.append(Polygon(line))
-        }
-        else if line.hasPrefix("v ")
-        {
-          vertices.append(Vertice(line))
-        }
+        polygons.append(Polygon(line))
       }
-      
-      let lightDirection = TUIVec3(x: -0.0, y: -0.5, z: -1)
-//      let lightDirection = TUIVec3(x: 0, y: 0, z: -1)
-      
+      else if line.hasPrefix("v ")
+      {
+        vertices.append(Vertice(line))
+      }
+    }
+    
+    let lightDirection = TUIVec3(x: -0.0, y: -0.5, z: -1)
+    
+    for shoeSkip in stride(from: 400.0, through: 0.0, by: -25)
+    {
       for polygon in polygons
       {
         var screenCoords = Array<TUIVec2>(repeating: TUIVec2(), count:  3)
         var worldCoords = Array<TUIVec3>(repeating: TUIVec3(), count: 3)
         
-        for j in 0..<3
+        for j in 0 ..< 3
         {
-          let v = vertices[polygon.indices[j].x - 1]
-
-          screenCoords[j] = TUIVec2(x: (v.x + 1) * center.x, y: (v.y + 1) * center.y)
+          let v = vertices[polygon.faces[j].face - 1]
+          screenCoords[j] = TUIVec2(x: (v.x + 1) * center.x, y: (v.y + 1) * center.y) * 1
           worldCoords[j] = TUIVec3(x: v.x, y: v.y, z: v.z)
+          // Move coords to center object
+          // screenCoords[j] = screenCoords[j] + TUIVec2(x: center.x / 2 + 20, y: 0)
         }
         
-        var n = (worldCoords[2] - worldCoords[0]) ** (worldCoords[1] - worldCoords[0])
-        n = n.normalize()
-        
-        let intensity = n * lightDirection
-        
+        let shoelace = Example.Wavefront.shoelace(screenCoords)
+        if shoelace < shoeSkip || shoelace > shoeSkip + 25  { continue }
+        let n = (worldCoords[2] - worldCoords[0]) ** (worldCoords[1] - worldCoords[0])
+        let intensity = n.normalize() * lightDirection
         if intensity > 0
         {
           let color = Ansi.Color(red: 0.98 * intensity,
-                                 green: 0.75 * intensity,
-                                 blue: 0.57 * intensity, alpha: 1)
+                                 green: 0.85 * intensity,
+                                 blue: 0.67 * intensity, alpha: 1)
           view.drawTriangle(vector1: screenCoords[0],
                             vector2: screenCoords[1],
-                            vector3: screenCoords[2], color: color)
+                            vector3: screenCoords[2], color: color, fill: true)
         }
-        
       }
-      
-      let renderParm = TUIRenderParameter(colorspace: .foreground256, composite: .average, style: .long)
-      
-      view.draw(parameters: renderParm)
     }
-    
+    let renderParm = TUIRenderParameter(colorspace: .foreground256,
+                                        composite: .average, style: .drawille)
+    view.draw(parameters: renderParm)
+    Ansi.Cursor.position().stdout()
+    Ansi.flush()
   }
 }
